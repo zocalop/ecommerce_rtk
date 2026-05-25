d762cd7939c61b3d1abd116c089826bbf519629c
zocalop <jcb2861@gmail.com>
2026-05-25 00:27:45 -0300

    Updated CartSlice.jsx in main

--- src/Components/CartSlice.jsx
+++ src/Components/CartSlice.jsx
@@ -6,7 +6,18 @@ const initialState = {
 };
 
 const CartSlice = createSlice({
- 
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItemToCart(state, action) {
+      const existingItem = state.cartItems.find(item => item.id === action.payload.id);
+      if (existingItem) {
+        existingItem.quantity +=1;
+      } else {
+        state.cartItems.push({ ...action.payload, quantity: 1});
+      }
+    },
+  } 
 });
 
 
