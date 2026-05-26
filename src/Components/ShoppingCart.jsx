08d313b5b179329fbb5b56e75d308765a7cbcadc
zocalop <jcb2861@gmail.com>
2026-05-26 02:50:00 -0300

    Updated ecommerce_rtk in main

--- src/Components/ShoppingCart.jsx
+++ src/Components/ShoppingCart.jsx
@@ -5,6 +5,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { removeItemFromCart, clearCart, increaseItemQuantity, decreaseItemQuantity } from './CartSlice.jsx';
 
 const ShoppingCart = () => {
+  const dispatch = useDispatch();
+    const cartItems = useSelector(state => state.cart.cartItems);
+    const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+  const handleRemoveItem = itemId => {
+    dispatch(removeItemFromCart(itemId));
+  };
+    
 
   return (
     <>
