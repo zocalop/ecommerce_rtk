239e9e6b79ea9240b1ab8c63248ae1ed25c4161b
zocalop <jcb2861@gmail.com>
2026-05-29 14:04:49 -0300

    Updated SuperCoin.jsx in main

--- src/Components/SuperCoin.jsx
+++ src/Components/SuperCoin.jsx
@@ -1,5 +1,18 @@
 
-import { useState } from 'react';
+import { useState, useSelector, useEffect } from 'react';
 
 const [superCoins, setSuperCoins] = useState(0);
+const cartItems = useSelector(state => state.cart.cartItems);
+const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
 
+useEffect(() => {
+  if (totalAmount >= 100 && totalAmount < 200) {
+    setSuperCoins(10);
+  } else if (totalAmount >= 200 && totalAmount < 300) {
+    setSuperCoins(20);
+  } else if (totalAmount >= 300) {
+    setSuperCoins(30);
+  } else {
+    setSuperCoins(0);
+  }
+}, [totalAmount]);
